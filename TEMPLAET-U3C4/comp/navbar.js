110ae23a3d347f3b8e711af1f3973327b7fa83ae
Mukesh Chouhan <95852087+Mukeshfw14586@users.noreply.github.com>
2022-01-17 12:35:28 +0530

    Add files via upload

---
+++ TEMPLAET-U3C4/comp/navbar.js
@@ -0,0 +1,9 @@
+function navbar() {
+    return `
+    // <div id="count"><a href="cart.html">💖Cart &nbsp;<span id="cartcount">0</span></a></div>
+    <div id="signup"><a href="signup.html">SignUp</a></div>
+    <div id="login"><a href="login.html">Login</a></div>
+    <div id="menu22"><a href="index.html">Menu</a></div>
+    <div id="wel">Welcome....!</div>`
+}
+export default navbar;
